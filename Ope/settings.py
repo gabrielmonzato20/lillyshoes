15097590e9ea8c0c49facd286b41a36a371e9fa1
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 01:08:00 -0400

    harusami final

--- Ope/settings.py
+++ Ope/settings.py
@@ -151,5 +151,3 @@ STATIC_ROOT =os.path.join(PROJECT_ROOT,'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_DIRS = [BASE_DIR+'/static']
-STATIC_ROOT = BASE_DIR+'/staticfiles'
