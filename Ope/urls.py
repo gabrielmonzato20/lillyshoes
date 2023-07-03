849187d2138ff774f24a99ac2c77e6fc599f03a3
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 01:30:21 -0400

    harusamifinalmode1

--- Ope/urls.py
+++ Ope/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include('Cliente.urls')),
@@ -25,3 +26,5 @@ urlpatterns = [
     path('pedidos/', include('Pedido.urls')),
     path('', include('Dashboard.urls')),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
