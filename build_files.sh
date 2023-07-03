2951ec3e59bfcaa96e2c76ee2b7df7ad0ff23fae
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 00:44:52 -0400

    hope

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
-pip install -r requirements.txt
+pip install -r requirements.txt -y
 
 # make migrations
-python3.9 manage.py migrate 
-python3.9 manage.py collectstatic
+python3.9 manage.py migrate -y
+python3.9 manage.py collectstatic -y
