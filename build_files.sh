c02123959b827022b05c222037e5634589a81ea9
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 00:40:24 -0400

    hope

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,6 @@
 # build_files.sh
-sudo apt-get install libpq-dev
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput
+
+# make migrations
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic
