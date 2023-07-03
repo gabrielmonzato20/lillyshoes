455495421ac42a0301b3f4097d4d6e961eb551ac
gabrielmonzato20 <gabrielmonzato20@gmail.com>
2023-07-03 00:50:31 -0400

    harusami

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 # build_files.sh
-pip install -r requirements.txt -y
+pip install -r requirements.txt 
 
 # make migrations
-python3.9 manage.py migrate -y
-python3.9 manage.py collectstatic -y
+python3.9 manage.py migrate 
+python3.9 manage.py collectstatic 
